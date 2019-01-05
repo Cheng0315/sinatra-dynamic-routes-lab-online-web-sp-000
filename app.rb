0f57e1f03cb56cfd2e4e553470830c66053446ce
Cheng Vue <cvu3@ucsc.edu>
2019-01-05 01:34:42 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,6 +15,6 @@ class App < Sinatra::Base
   get "/say/:number/:phrase" do
     @number = params[:number]
     @phrase = params[:phrase]
-    @number.to_i.times {|@phrase| "#{@phrase}"}
+    @number.to_i.times {|@phrase| @phrase}
   end
 end
