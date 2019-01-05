a7d8d1137edcb9f8fde5356728367ffad4a8d3f9
Cheng Vue <cvu3@ucsc.edu>
2019-01-05 02:06:12 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -16,7 +16,8 @@ class App < Sinatra::Base
     @number = params[:number]
     @phrase = params[:phrase]
 
-    <% params[:number].to_i.times { |@phrase| "#{@phrase}" }%>
-
+    <% params[:number].to_i.times do %>
+    "#{@phrase}"
+    <% end %>
   end
 end
